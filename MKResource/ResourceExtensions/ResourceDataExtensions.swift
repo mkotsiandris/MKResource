3e27299ba7a84da1fdc6b7440171d55755ae2d9f
Marios Kotsiandris <marioskots@gmail.com>
2019-04-12 12:27:01 +0100

    Minor change for returning optional on Data

--- MKResource/ResourceExtensions/ResourceDataExtensions.swift
+++ MKResource/ResourceExtensions/ResourceDataExtensions.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 public extension Resourcable where ResourceType == Data {
-    func load(bundle: Bundle = Bundle.main) -> Data {
-        guard let path = Bundle.main.url(forResource: fileName, withExtension: fileExtension) else { return Data() }
-        return try! Data(contentsOf: path)
+    func load(bundle: Bundle = Bundle.main) -> Data? {
+        guard let path = Bundle.main.url(forResource: fileName, withExtension: fileExtension) else { return nil }
+        return try? Data(contentsOf: path)
     }
 }
